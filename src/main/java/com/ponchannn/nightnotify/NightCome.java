0c363066e44200a9974c5c123ea1e1a6dfc43487
ponCHANNN <mizuki.7302@gmail.com>
2024-08-03 17:09:09 +0900

    change sounds n add notify when join n chat

--- src/main/java/com/ponchannn/nightnotify/NightCome.java
+++ src/main/java/com/ponchannn/nightnotify/NightCome.java
@@ -6,7 +6,7 @@ import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
-import static org.bukkit.Sound.ENTITY_ENDER_DRAGON_GROWL;
+import static org.bukkit.Sound.ENTITY_WOLF_HOWL;
 
 public class NightCome extends BukkitRunnable {
 
@@ -18,7 +18,7 @@ public class NightCome extends BukkitRunnable {
             if (time >= 12541 && time <= 12541 + 194) {
                 for (Player player : world.getPlayers()) {
                     player.sendTitle("", ChatColor.AQUA + "夜だよ！寝よう！！！", 10, 60, 20);
-                    player.playSound(player.getLocation(), ENTITY_ENDER_DRAGON_GROWL, 1.0f, 1.0f);
+                    player.playSound(player.getLocation(), ENTITY_WOLF_HOWL, 1.0f, 1.0f);
                 }
 
 
--- src/main/java/com/ponchannn/nightnotify/NightNotify.java
+++ src/main/java/com/ponchannn/nightnotify/NightNotify.java
@@ -1,21 +1,13 @@
 package com.ponchannn.nightnotify;
 
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import static org.bukkit.Sound.ENTITY_ENDER_DRAGON_GROWL;
 
 public final class NightNotify extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-        getServer().getPluginManager().registerEvents(new onJoin(), this);
-        Player player = Bukkit.getPlayer("ponCHANNN");
-        player.playSound(player.getLocation(), ENTITY_ENDER_DRAGON_GROWL, 0.5f, 1.0f);
-        player.sendTitle("", ChatColor.AQUA + "夜だよ！寝よう！！！", 10, 60, 20);
+        getServer().getPluginManager().registerEvents(new onEvent(), this);
         new NightCome().runTaskTimer(this, 0, 20 * 10);
 
     }
